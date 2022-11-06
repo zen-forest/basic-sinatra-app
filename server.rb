e83662d86eaf0556789239a391c04032767c1fb6
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-11-06 10:39:34 -0500

    routes and responses for Index and Show

--- server.rb
+++ server.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
-get '/' do
-  "Welcome to booklist"
+# Model
+posts = [{title: "First Post", body: "content of first post"}]
+
+# Index route
+get '/posts' do
+  return posts.to_json
+end
+
+# Show route
+get '/posts/:id' do
+  # return a particular post as JSON based on the id param
+  # params always come to a string so we need to convert to an integer
+  id = params["id"].to_i
+  return posts[id].to_json
 end
