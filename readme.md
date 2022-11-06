695f518c476e9ba901df46057f9790413ce63a40
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-11-06 12:45:23 -0500

    CRUD implemented, nothing too crazy here

---
+++ readme.md
@@ -0,0 +1,10 @@
+
+## Restful Conventions
+| Name of Route | Request Method | Endpoint | Result |
+|:----------------|:------------------|:---------|:-------|
+|Index|GET|/model|Returns list of all items|
+|Show|GET|/model/:id|Returns list with matching ID|
+|Create|POST|/model|creates a new item, returns item or confirmation|
+|Update|PUT|/model/:id|Updated item with matching ID|
+|Destroy|DELETE|/model/:id|Delete items with matching ID|
+
--- server.rb
+++ server.rb
@@ -1,17 +1,64 @@
 require 'sinatra'
+require 'json'
 
-# Model
+## Model/Dataset
+
 posts = [{title: "First Post", body: "content of first post"}]
 
-# Index route
-get '/posts' do
-  return posts.to_json
+## Custom Method for Getting Request body
+def getBody (req)
+    ## Rewind the body in case it has already been read
+    req.body.rewind
+    ## parse the body
+    return JSON.parse(req.body.read)
 end
 
-# Show route
+
+## Index route
+get '/posts' do
+    # Return all the posts as JSON
+    return posts.to_json
+end
+
+## Show Route
 get '/posts/:id' do
-  # return a particular post as JSON based on the id param
-  # params always come to a string so we need to convert to an integer
-  id = params["id"].to_i
-  return posts[id].to_json
+    # return a particular post as json based on the id param from the url
+    # Params always come to a string so we convert to an integer
+    id = params["id"].to_i
+    return posts[id].to_json
+end
+
+## Create Route
+post '/posts' do
+    # Pass the request into the custom getBody function
+    body = getBody(request)
+    # create the new post
+    new_post = {title: body["title"], body: body["body"]}
+    # push the new post into the array
+    posts.push(new_post)
+    # return the new post
+    return new_post.to_json
+end
+
+## Update Route
+put '/posts/:id' do
+    # get the id from params
+    id = params["id"].to_i
+    # get the request body
+    body = getBody(request)
+    #update the item in question
+    posts[id][:title] = body["title"]
+    posts[id][:body] = body["body"]
+    #return the updated post
+    return posts[id].to_json
+end
+
+## Destroy Route
+delete '/posts/:id' do
+    # get the id from params
+    id = params["id"].to_i
+    # delete the item
+    post = posts.delete_at(id)
+    # return the deleted item as json
+    return post.to_json
 end
